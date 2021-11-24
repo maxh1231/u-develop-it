cb5cde496a1102806152dea4af0f5648c7fce703
maxh1231 <maxhumpherys@gmail.com>
2021-11-24 00:02:21 -0700

    modularize

---
+++ db/connection.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    // Your MySQL username,
+    user: 'root',
+    // Your MySQL password
+    password: '1234',
+    database: 'election'
+});
+
+module.exports = db;
